65c93918cdf1c0df5b8bf3b5e360f25a367840ec
Chol-Mach-Kuol <cholmachkuol331@gmail.com>
2026-06-23 17:16:03 +0200

    api_advanced: top_ten — print string None, simplify logic

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,20 +4,18 @@ import requests
 
 
 def top_ten(subreddit):
-    """Print titles of the first 10 hot posts for a subreddit."""
+    """Print titles of the first 10 hot posts for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "linux:api_advanced:v1.0 (by /u/api_advanced)"}
-    params = {"limit": 10}
     response = requests.get(
-        url, headers=headers, params=params, allow_redirects=False
+        url,
+        headers=headers,
+        params={"limit": 10},
+        allow_redirects=False
     )
-    if response.status_code != 200:
-        print(None)
-        return
-    data = response.json().get("data")
-    if data is None:
-        print(None)
-        return
-    posts = data.get("children", [])
-    for post in posts[:10]:
-        print(post.get("data", {}).get("title"))
+    if response.status_code == 200:
+        posts = response.json().get("data", {}).get("children", [])
+        for post in posts[:10]:
+            print(post.get("data", {}).get("title"))
+    else:
+        print("None")
